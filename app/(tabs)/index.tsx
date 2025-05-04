11d4818f2f35ee0d0f710c2868c0705472542db5
Tim Blais <blaist@merrimack.edu>
2025-05-04 13:31:29 -0400

    added ability to place sticker on image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,15 @@
+import { type ImageSource } from "expo-image"
 import * as ImagePicker from "expo-image-picker"
 import { useState } from "react"
 import { StyleSheet, View } from "react-native"
 
-import { type ImageSource } from "expo-image"
-
 import Button from "@/components/Button"
 import CircleButton from "@/components/CircleButton"
+import EmojiList from "@/components/EmojiList"
 import EmojiPicker from "@/components/EmojiPicker"
+import EmojiSticker from "@/components/EmojiSticker"
 import IconButton from "@/components/IconButton"
 import ImageViewer from "@/components/ImageViewer"
-
-import EmojiList from "@/components/EmojiList"
 
 const PlaceholderImage = require("@/assets/images/background-image.png")
 
@@ -62,6 +61,9 @@ export default function Index() {
           imgSource={PlaceholderImage}
           selectedImage={selectedImage}
         />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />
+        )}
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
